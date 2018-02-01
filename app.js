2d6300253905722931f3c8c4a32f636a8916fa21
nikolalsvk <nikolaseap@gmail.com>
2018-02-01 15:12:50 +0100

    List projects from GitHub

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let appRoot = new Vue({
+  el: "#app-root",
+  data: {
+    rawProjects: [],
+    projectsCount: 0
+  },
+  computed: {
+    projects: function() {
+      return this.rawProjects.map(p => p.name)
+    }
+  },
+
+  created() {
+    axios
+      .get("https://api.github.com/search/repositories?q=topic:os-website+org:renderedtext")
+      .then(response => {
+        const data = response.data
+
+        this.rawProjects = data.items
+        this.projectsCount = data.total_count
+      })
+  }
+})
